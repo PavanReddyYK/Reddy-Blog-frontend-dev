4adaf79d0367a674341f7d85429466e8b1eae884
PavanReddyYk <pavanykreddy24@gmail.com>
2024-02-08 14:57:04 +0530

    added routes with the help of react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div className='text-justify'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/SignIn' element={<SignIn/>}/>
+        <Route path='/SignUp' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
